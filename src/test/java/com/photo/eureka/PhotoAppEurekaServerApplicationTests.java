873b77ed18a3895abfe7d6450aa32da501ff4a85
ashish <Ashish.Anand@ISIPL-PUNV-L163.corp.intelliswift.com>
2020-01-14 16:59:29 +0530

    Eureka server commit

---
+++ src/test/java/com/photo/eureka/PhotoAppEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
